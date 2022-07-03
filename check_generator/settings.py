90c612624e9910651c29eb583d51ddd043f2f00a
Fdrv <oldgot@gmail.com>
2022-07-03 15:20:10 +0300

    Add html templates

--- check_generator/settings.py
+++ check_generator/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'check_generator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
