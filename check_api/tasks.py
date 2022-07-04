acd497962a07fa54430c76f7ee8341894ecb031b
Fdrv <oldgot@gmail.com>
2022-07-04 15:30:11 +0300

    Add changing check status  without rendering pdf when file already exists

--- check_api/tasks.py
+++ check_api/tasks.py
@@ -1,17 +1,3 @@
-import os
-import django
-
-from django.db import connection
-from django.template import Context, Template
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "check_generator.settings")
-django.setup()
-
-# import models AFTER DJANGO SETTINGS INIT!
-from check_api.models import *
-
-
 import json
 import base64
 from pathlib import Path
@@ -20,12 +6,12 @@ import requests
 
 from django.template.loader import render_to_string
 from django.conf import settings
-from django.core.files import File
 from django.core.files.base import ContentFile
 
 from check_api.models import Check
 
 WKHTMLTOPDF = settings.WKHTMLTOPDF
+
 
 # def render_html(template, order):
 #     templates = {
@@ -41,7 +27,7 @@ def generate_pdf_file(check, filename):
         'contents': base64.b64encode(bytes(check, 'utf8')).decode('utf-8'),
     }
     headers = {
-        'Content-Type': 'application/json',  # This is important
+        'Content-Type': 'application/json',
     }
     response = requests.post(url, data=json.dumps(data), headers=headers)
 
@@ -55,18 +41,22 @@ def process_pdf(check):
         'KN': 'kitchen_check.html',
         'CL': 'client_check.html',
     }
+    check_type_names = {
+        'KN': 'kitchen',
+        'CL': 'client',
+    }
 
-
-    file_name = f'{check.order["id"]}_{check.type}.pdf'
-
+    file_name = f'{check.order["id"]}_{check_type_names[check.type]}.pdf'
     path = Path(settings.MEDIA_ROOT / 'pdf' / file_name)
     if path.is_file():
-        return
+        check.status = Check.RENDERED
+        check.save()
+        return None
 
     rendered_check = render_to_string(templates[check.type], context=check.order)
-
     file = ContentFile(generate_pdf_file(rendered_check, file_name))
-    # check.pdf_file = str(path)
+
     check.pdf_file.save(file_name, file, save=False)
-    check.status = 'RE'
+    check.status = Check.RENDERED
     check.save()
+    return None
