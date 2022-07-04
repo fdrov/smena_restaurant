386d0788af6fb604942614f3536e2e83d00bcad9
Fdrv <oldgot@gmail.com>
2022-07-04 15:23:48 +0300

    Add filtering to admin site

--- check_api/admin.py
+++ check_api/admin.py
@@ -6,9 +6,12 @@ from .models import Printer, Check
 @admin.register(Printer)
 class PrinterAdmin(admin.ModelAdmin):
     list_display = ['name', 'check_type', 'point_id']
+    list_filter = ['check_type', 'point_id']
+
 
 
 @admin.register(Check)
 class CheckAdmin(admin.ModelAdmin):
     list_display = ['pk', 'status', 'printer_id', 'type']
+    list_filter = ['status', 'printer_id', 'type']
 
